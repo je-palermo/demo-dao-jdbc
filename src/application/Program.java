4df2af4f4107012d57275c692ffeec6d4d9c7d36
je-palermo <jpalermo695@gmail.com>
2021-06-01 09:36:23 -0300

    Classe Seller implementada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,25 @@
 package application;
 
+import java.util.Date;
+import java.util.Locale;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		
+		
 		Department dep = new Department(1, "Books");
-		System.out.println(dep);
-
+		
+		Seller sel = new Seller(21, "Fudencio", "fudencio.com", new Date(), 3000.0, dep);
+		
+		System.out.println(sel);
+		
+		
 	}
 
 }
